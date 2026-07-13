e7fad88696fffa7f272931897da506ad0166d1fd
Ryan <253573263+ryanh123420@users.noreply.github.com>
2026-07-13 17:27:34 -0400

    Changed the UX of postout create to use two button for selecting current vs future reset rather than a drop down

--- src/main/java/com/ryanh/agent_discord_bot/listener/PostOutListener.java
+++ src/main/java/com/ryanh/agent_discord_bot/listener/PostOutListener.java
@@ -41,16 +41,13 @@ public class PostOutListener extends ListenerAdapter {
                             .build())
                     .addComponents(
                             ActionRow.of(
-                                    StringSelectMenu.create("postout-selectreset")
-                                    .addOption("This Reset", "postout-thisreset",
-                                            "Week of "
+                                    Button.primary("postout-thisreset", "This Week, Week of "
                                             + postOutService.getReset().getMonthValue() + "/"
-                                            + postOutService.getReset().getDayOfMonth())
-                                    .addOption("Future Date", "postout-futurereset",
-                                            "After this reset at a later date.")
-                                    .build()),
+                                            + postOutService.getReset().getDayOfMonth()),
+                                    Button.primary("postout-futurereset", "Later Week")
+                            ),
                             ActionRow.of(
-                                    Button.danger("postout-cancel", "Cancel")
+                                    Button.danger("postout-create-cancel", "Cancel")
                             )
                     )
                     .setEphemeral(true)
@@ -124,45 +121,7 @@ public class PostOutListener extends ListenerAdapter {
 
     @Override
     public void onStringSelectInteraction(StringSelectInteractionEvent event) {
-        if(event.getComponentId().equals("postout-selectreset")) {
-            String selected = event.getValues().getFirst();
-            if(selected.equals("postout-thisreset")) {
-                StringSelectMenu.Builder menu = StringSelectMenu.create("postout-thisreset-selectdays")
-                        .setMinValues(1);
-
-                for (PostOutService.RaidDay day: postOutService.validMenuOptions()) {
-                    menu.addOption(day.label(), day.value(),
-                            day.date().getMonth().getValue() + "/" + day.date().getDayOfMonth());
-                }
-                menu.setMaxValues(menu.getOptions().size());
-
-                event.editMessageEmbeds(EmbedUtility.info("Create a Post Out",
-                                "Select which days:").build())
-                        .setComponents(
-                                ActionRow.of(
-                                        menu.build()
-                                ),
-                                ActionRow.of(
-                                        Button.primary("postout-create-confirm", "Confirm"),
-                                        Button.danger("postout-create-cancel", "Cancel")
-                                )
-                        ).queue();
-            }
-            else if (selected.equals("postout-futurereset")) {
-                TextInput dateInput = TextInput.create("dateInput", TextInputStyle.SHORT)
-                        .setPlaceholder("Example format: 4/20, 6/7, 6/9")
-                        .setRequired(true)
-                        .build();
-
-                Modal modal = Modal.create("postout-futurereset-datemodal", "Create a Post Out")
-                        .addComponents(Label.of("Enter month/day, separated by commas", dateInput))
-                        .build();
-
-                event.replyModal(modal).queue();
-            }
-
-        }
-        else if(event.getComponentId().equals("postout-thisreset-selectdays")) {
+        if(event.getComponentId().equals("postout-thisreset-selectdays")) {
             daySelections.put(event.getUser().getId(), event.getValues());
             event.deferEdit().queue();
         }
@@ -175,7 +134,41 @@ public class PostOutListener extends ListenerAdapter {
 
     @Override
     public void onButtonInteraction(ButtonInteractionEvent event) {
-        if(event.getComponentId().equals("postout-create-cancel")) {
+        if(event.getComponentId().equals("postout-thisreset")) {
+            StringSelectMenu.Builder menu = StringSelectMenu.create("postout-thisreset-selectdays")
+                    .setMinValues(1);
+
+            for (PostOutService.RaidDay day: postOutService.validMenuOptions()) {
+                menu.addOption(day.label(), day.value(),
+                        day.date().getMonth().getValue() + "/" + day.date().getDayOfMonth());
+            }
+            menu.setMaxValues(menu.getOptions().size());
+
+            event.editMessageEmbeds(EmbedUtility.info("Create a Post Out",
+                            "Select which days:").build())
+                    .setComponents(
+                            ActionRow.of(
+                                    menu.build()
+                            ),
+                            ActionRow.of(
+                                    Button.primary("postout-create-confirm", "Confirm"),
+                                    Button.danger("postout-create-cancel", "Cancel")
+                            )
+                    ).queue();
+        }
+        else if (event.getComponentId().equals("postout-futurereset")) {
+            TextInput dateInput = TextInput.create("dateInput", TextInputStyle.SHORT)
+                    .setPlaceholder("Example format: 4/20, 6/7, 6/9")
+                    .setRequired(true)
+                    .build();
+
+            Modal modal = Modal.create("postout-futurereset-datemodal", "Create a Post Out")
+                    .addComponents(Label.of("Enter month/day, separated by commas", dateInput))
+                    .build();
+
+            event.replyModal(modal).queue();
+        }
+        else if(event.getComponentId().equals("postout-create-cancel")) {
             daySelections.remove(event.getUser().getId());
             event.editMessageEmbeds(EmbedUtility.error("Create a Post Out",
                             "Post Out canceled").build())
